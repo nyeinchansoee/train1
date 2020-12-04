59e80601ddd9948c8c549b680e6d69c18599bdff
nyeinchansoee <nyeinchansoe.mm@gmail.com>
2020-12-04 19:21:22 +0630

    ver1

---
+++ android/app/src/main/kotlin/com/example/train1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.train1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.greenAccent,
+          appBar: AppBar(
+            backgroundColor: Colors.lightBlueAccent,
+            title: Text('Moehtet'),
+          ),
+          body: Center(
+            child: Image.network(
+                'https://library.kissclipart.com/20190827/yte/kissclipart-child-animated-cartoon-baby-cartoon-nose-11b208f06745b77b.png'),
+          ),
+        ),
+
+        // home: Center(
+        //   child: Text('Moehtet'),
+        // ),
+      ),
+    );
